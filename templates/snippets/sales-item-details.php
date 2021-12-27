d184bd3b700ea15fd88fbd706b5e96d49eea328c
zub333 <admin.webtime@gmail.com>
2021-12-27 21:45:05 +0300

    Gift Card: multiple selection fix

--- templates/snippets/sales-item-details.php
+++ templates/snippets/sales-item-details.php
@@ -4,7 +4,6 @@ namespace DanceStudioManager;
 $sales_item_id = App::GetApi()->GetIdParam();
 $sales_item_full_info = App::GetClient()->GetController('checkout')->GetSalesItemInfo($sales_item_id);
 
-$sales_item_id = App::GetApi()->GetIdParam();
 if (defined('DSM_OC_BUY_ITEM_PAGE_VIEW_TYPE') && DSM_OC_BUY_ITEM_PAGE_VIEW_TYPE == '1')
 	$sales_products = $sales_item_full_info['sales_items']['item'];
 else
@@ -19,9 +18,19 @@ else
 		<p><?php echo $sales_item['DESCRIPTION']; ?></p>
 		<p>Price: <b><?php echo DSM_CURRENCY_SIGN; ?><?php echo $sales_item['PRICE']; ?></b></p>
 		<br>
+		<?php if ($sales_item['TYPE'] != 'gift_card'): ?>
 		<?php foreach ($sales_item_full_info['students'] as $student): ?>
 			<?php include plugin_dir_path( __FILE__ ) . 'select-sales-item.php'; ?>
 		<?php endforeach; ?>
+		<?php else: ?>
+        <button class="btn btn-success select-product dsm_ajax_tab" type="button"
+			id="quant-<?php echo $sales_item['ID']; ?>"
+			dsm_obj="checkout" dsm_method="SubmitSalesItem"
+			dsm_sales_item_id="<?php echo $sales_item['ID']; ?>" dsm_quantity="1"
+        	dsm_activation_date="<?php echo date("M j, Y"); ?>">
+        	<i class="fa fa-shopping-cart"></i> <span>Add to Cart</span>
+		</button>
+		<? endif; ?>
 		<br>
 </div>
 <?php endforeach; ?>
